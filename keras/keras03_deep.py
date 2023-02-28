dd97f53f9222848bdaaaeb6c73f3c878703810be
CHOHYUNKYU <ksl103177@gmail.com>
2023-02-28 12:00:49 +0900

    keras

--- keras/keras03_deep.py
+++ keras/keras03_deep.py
@@ -1,4 +1,4 @@
-#1 
+#1 데이터
 import numpy as np
 x = np.array([1,2,3])
 y = np.array([1,2,3])
@@ -9,10 +9,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 model = Sequential()
-model.add(Dense(1, input_dim=1))
+model.add(Dense(3, input_dim=1))
+model.add(Dense(5))
+model.add(Dense(20))
+model.add(Dense(3))
+model.add(Dense(1))
 
 #3 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
 model.fit(x,y,epochs=100)
 
-# loss 27.0701
+#loss1 : 1.1604
+#loss2 : 0.0086
+#loss3 : 0.0011

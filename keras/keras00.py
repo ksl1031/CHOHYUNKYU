784b80a6431580764383f0047f614a4633e5c6f8
CHOHYUNKYU <ksl103177@gmail.com>
2023-03-01 13:39:27 +0900

    keras

--- keras/keras00.py
+++ keras/keras00.py
@@ -3,7 +3,7 @@ import numpy as np
 x = np.array([1,2,3])
 y = np.array([1,2,3])
 
-#2모델구성
+#2모델 구성
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -11,18 +11,22 @@ from tensorflow.keras.layers import Dense
 model = Sequential()
 model.add(Dense(2, input_dim = 1))
 model.add(Dense(4))
+model.add(Dense(7))
 model.add(Dense(6))
 model.add(Dense(3))
 model.add(Dense(1))
 
-#3컴파일, 훈련
+#3컴파일 ,훈련
 model.compile(loss = "mse", optimizer = "adam")
-model.fit(x,y,epochs=600)
+model.fit(x, y, epochs = 600)
 
 #4평가, 예측
+loss = model.evaluate(x, y)
+print("loss : ", loss)
 
-loss = model.evaluate(x,y)
-print("loss :  ", loss)
+reslut = model.predict([7])
+print("7의 예측값은 : ", reslut)
 
-reslut = model.predict([6])
-print("6의 예측값 : ", reslut)
+#1 7.000001
+#2 6.9999423
+#3 6.9558496

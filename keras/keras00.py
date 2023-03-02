fe88d2c8144870d400210c0642660dae437cec85
CHOHYUNKYU <ksl103177@gmail.com>
2023-03-02 19:05:39 +0900

    keras

--- keras/keras00.py
+++ keras/keras00.py
@@ -3,14 +3,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 #1 데이터
-x = np.array([[1,2],
-             [3,4],
-             [5,6],
-             [7,8],
-             [9,10]])
-print(x)
-print(x.shape)
+x = np.array([1,2,3,4,5,6,7,8,9,10])
+y = np.array([1,2,3,4,5,6,7,8,9,10])
 
-#2 모델 구성
+x_train = np.array([1,2,3,4,5,6,7])
+y_train = np.array([1,2,3,4,5,6,7])
+x_test = np.array([8,9,10])
+y_test = np.array([8,9,10])
+
+#2 모델구성
+model = Sequential()
+model.add(Dense(10, input_dim = 1))
+model.add(Dense(1))
+
 #3 컴파일, 훈련
+model.compile(loss = 'mse', optimizer = 'adam')
+model.fit(x_train, y_train, epochs = 300, batch_size = 2)
+
 #4 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print("loss : ", loss)
+result = model.predict([11])
+print("11의 예측값은 : ", result)
--- keras/keras08_train_test1.py
+++ keras/keras08_train_test1.py
@@ -7,7 +7,7 @@ x = np.array([1,2,3,4,5,6,7,8,9,10])
 y = np.array([10,9,8,7,6,5,4,3,2,1,]) # 마지막 숫자뒤에 ,가 있어도 문제 X
 # print(x)
 # print(y)
-x_train =np.array([1,2,3,4,5,6,7])
+x_train =np.array([1,2,3,4,5,6,7]) # train : 훈련
 y_train =np.array([1,2,3,4,5,6,7])
 x_test = np.array([8,9,10])
 y_test = np.array([8,9,10])

a4a525e37f52144692bf5480f2cbd955c6546e1d
CHOHYUNKYU <ksl103177@gmail.com>
2023-03-02 10:38:15 +0900

    keras

--- keras/keras00.py
+++ keras/keras00.py
@@ -3,24 +3,25 @@ import numpy as np
 x = np.array([1,2,3])
 y = np.array([1,2,3])
 
-#2 모델구성
+#2 모델 구성
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+
 model = Sequential()
-model.add(Dense (2, input_dim =1))
-model.add(Dense(3))
-model.add(Dense(5))
+model.add(Dense(2, input_dim = 1))
 model.add(Dense(4))
+model.add(Dense(9))
+model.add(Dense(6))
 model.add(Dense(1))
 
 #3 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam')
-model.fit(x,y,epochs = 500, batch_size = 2)
+model.compile(loss = "mse", optimizer = "adam")
+model.fit(x, y, epochs = 600)
 
-#4 평가, 예측
-loss = model.evaluate(x,y) #평가
+#4 평가, 에측
+loss = model.evaluate(x, y)
 print("loss : ", loss)
-reslut = model.predict([5]) #예측
-print("5의 예측값 : ", reslut)
 
+result = model.predict([9])
+print("9의 예측값 : ", result)
--- keras/keras04_.py
+++ keras/keras04_.py
@@ -10,9 +10,9 @@ model = Sequential()
 model.add(Dense(3, input_dim=1))
 model.add(Dense(5))
 model.add(Dense(6))
-model.add(Dense(7))
-model.add(Dense(6))
 model.add(Dense(5))
+model.add(Dense(4))
+model.add(Dense(2))
 model.add(Dense(1))
 
 #3 컴파일, 훈련

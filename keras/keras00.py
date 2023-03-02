4cdc1e0632893d4b31c58cb2fefd681481326699
CHOHYUNKYU <ksl103177@gmail.com>
2023-03-02 23:15:08 +0900

    keras

--- keras/keras00.py
+++ keras/keras00.py
@@ -11,7 +11,7 @@ y_train = np.array([1,2,3,4,5,6,7])
 x_test = np.array([8,9,10])
 y_test = np.array([8,9,10])
 
-#2 모델구성
+#2 모델 구성
 model = Sequential()
 model.add(Dense(10, input_dim = 1))
 model.add(Dense(1))
@@ -24,4 +24,4 @@ model.fit(x_train, y_train, epochs = 300, batch_size = 2)
 loss = model.evaluate(x_test, y_test)
 print("loss : ", loss)
 result = model.predict([11])
-print("11의 예측값은 : ", result)
+print("11의 예측값 : ", result)

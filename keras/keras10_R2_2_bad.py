ed8c19669c21a68817bb65700ab8c0fb99a13921
CHOHYUNKYU <ksl103177@gmail.com>
2023-03-04 13:31:18 +0900

    keras

--- keras/keras10_R2_2_bad.py
+++ keras/keras10_R2_2_bad.py
@@ -9,8 +9,4 @@ x = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 y = np.array([1,2,4,3,5,7,9,3,8,12,13,8,14,15,9,6,17,23,21,20])
 
 x_train,x_test,y_train,y_test = train_test_split(x, y, # x = x_train, x_test # y = y_train, y_test로 분류 된다. x,y 위치가 바뀌어도 괜찮음
-<<<<<<< HEAD
-        train_size=0.8,shuffle=True,random_state=1234)
-=======
-        train_size=0.8,shuffle=True,random_state=1234)
->>>>>>> 377b3780bdbe0794f2a4d06e31736c5125d5fd6c
+train_size=0.8,shuffle=True,random_state=1234)
